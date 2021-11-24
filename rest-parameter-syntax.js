0ce5eb3e7f8468423e41e9fbf10ea6b914f9d5ec
Iuliia_Konovalova <JuliiaKonn@protonmail.com>
2021-11-24 05:21:44 +0000

    Add rest parameter challenge solution

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -5,6 +5,29 @@
 
 // Regular function call 
 
+const sumAll = (a, b, c) => a + b + c
+
+let sumThreeNum = sumAll(1, 2, 3)
+
+console.log("Sum 3 numbers: ", sumThreeNum)
+
 // Extra arguments are ignored
 
+
+let sumSixNum = sumAll(1, 2, 3, 4, 5, 6)
+
+console.log("Sum 6 numbers: ", sumSixNum)
+
 // Function using ...rest
+
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c
+    for (let i of rest) {
+        sum += i
+    }
+    return sum
+}
+
+let sumTenNum = sumRest(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+console.log("Sum 10 numbers: ", sumTenNum)
